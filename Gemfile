e4c4540d8d89bef7d557356827992a486c2f5b8c
yuuki0310 <fujikawak0310@yahoo.co.jp>
2023-05-23 19:26:27 +0900

    検索結果のページンング追加

--- Gemfile
+++ Gemfile
@@ -57,6 +57,9 @@ gem 'dotenv-rails'
 # APIの操作
 gem 'httparty'
 
+# ページング
+gem 'kaminari','~> 1.2.1'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,13 +5,16 @@ class RestaurantsController < ApplicationController
   end
 
   def index
+    # hotpepper_api呼び出し
     api_key = ENV['HOTPEPPER_API']
     lat = params[:lat]
     lng = params[:lng]
     url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{api_key}&lat=#{lat}&lng=#{lng}&format=json"
     response = HTTParty.get(url)
     data = JSON.parse(response.body)
-    @shops = data['results']['shop']
+
+    # ページング
+    @shops = Kaminari.paginate_array(data['results']['shop']).page(params[:page]).per(10)
   end
 
   def show
