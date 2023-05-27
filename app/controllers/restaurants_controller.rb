6afef44e9cae00204991f95631f62cc6be8a8080
yuuki0310 <fujikawak0310@yahoo.co.jp>
2023-05-27 17:22:09 +0900

    検索結果に地図を追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,26 +1,38 @@
 class RestaurantsController < ApplicationController
-  require 'httparty'
+  require "httparty"
 
   def search
   end
 
   def index
     # hotpepper_api呼び出し
-    api_key = ENV['HOTPEPPER_API']
-    lat = params[:lat]
-    lng = params[:lng]
-    range = params[:range]
-    url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{api_key}&lat=#{lat}&lng=#{lng}&range=#{range}&format=json"
+    api_key = ENV["HOTPEPPER_API"]
+    url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{api_key}&lat=#{params[:lat]}&lng=#{params[:lng]}&range=#{params[:range]}&format=json"
     response = HTTParty.get(url)
     data = JSON.parse(response.body)
 
     # ページング
-    @shops = Kaminari.paginate_array(data['results']['shop']).page(params[:page]).per(10)
+    @shops = Kaminari.paginate_array(data["results"]["shop"]).page(params[:page]).per(5)
+
+    # 検索結果の地図に描写する際のパラメーターをjsonで渡す
+    shop_map_informations = data["results"]["shop"].map do |shop|
+      {
+        "name" => shop["name"],
+        "address" => shop["address"],
+        "lat" => shop["lat"],
+        "lng" => shop["lng"],
+      }
+    end
+    @shop_map_informations_json = shop_map_informations.to_json
   end
 
   def show
-  end
+    # hotpepper_api呼び出し
+    api_key = ENV["HOTPEPPER_API"]
+    url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{api_key}&id=#{params[:id]}&format=json"
+    response = HTTParty.get(url)
+    data = JSON.parse(response.body)
 
-  def create
+    @shop = data["results"]["shop"][0]
   end
 end
