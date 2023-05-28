dcd46e850c806a6d8a9818d5fe5ba68ef7227eb5
yuuki0310 <fujikawak0310@yahoo.co.jp>
2023-05-28 21:19:05 +0900

    apiでの検索結果の出力を100件に設定

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,12 +7,12 @@ class RestaurantsController < ApplicationController
   def index
     # hotpepper_api呼び出し
     api_key = ENV["HOTPEPPER_API"]
-    url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{api_key}&lat=#{params[:lat]}&lng=#{params[:lng]}&range=#{params[:range]}&format=json"
+    url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{api_key}&lat=#{params[:lat]}&lng=#{params[:lng]}&range=#{params[:range]}&count=100&format=json"
     response = HTTParty.get(url)
     data = JSON.parse(response.body)
 
     # ページング
-    @shops = Kaminari.paginate_array(data["results"]["shop"]).page(params[:page]).per(5)
+    @shops = Kaminari.paginate_array(data["results"]["shop"]).page(params[:page]).per(10)
 
     # 検索結果の地図に描写する際のパラメーターをjsonで渡す
     @shop_map_informations = data["results"]["shop"].map do |shop|
