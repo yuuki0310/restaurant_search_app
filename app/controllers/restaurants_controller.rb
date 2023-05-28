d116e3aca13e1a7213fea111cfea504f5495eb5a
yuuki0310 <fujikawak0310@yahoo.co.jp>
2023-05-28 23:16:35 +0900

    位置情報が取得できないときの処理を追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,28 +5,33 @@ class RestaurantsController < ApplicationController
   end
 
   def index
-    # hotpepper_api呼び出し
-    api_key = ENV["HOTPEPPER_API"]
-    url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{api_key}&lat=#{params[:lat]}&lng=#{params[:lng]}&range=#{params[:range]}&count=100&format=json"
-    response = HTTParty.get(url)
-    data = JSON.parse(response.body)
-
-    # ページング
-    @shops = Kaminari.paginate_array(data["results"]["shop"]).page(params[:page]).per(10)
-
-    # 検索結果の地図に描写する際のパラメーターをjsonで渡す
-    @shop_map_informations = data["results"]["shop"].map do |shop|
-      {
-        "name" => shop["name"],
-        "lat" => shop["lat"],
-        "lng" => shop["lng"],
-        "img" => shop["photo"]["pc"]["s"],
-        "id" => shop["id"],
-      }
+    if params[:lat].empty? || params[:lng].empty?
+      # inputにlatlngが入れられる前に検索をかけられた時
+      redirect_to root_path
+    else
+      # hotpepper_api呼び出し
+      api_key = ENV["HOTPEPPER_API"]
+      url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{api_key}&lat=#{params[:lat]}&lng=#{params[:lng]}&range=#{params[:range]}&count=100&format=json"
+      response = HTTParty.get(url)
+      data = JSON.parse(response.body)
+  
+      # ページング
+      @shops = Kaminari.paginate_array(data["results"]["shop"]).page(params[:page]).per(10)
+  
+      # 検索結果の地図に描写する際のパラメーターをjsonで渡す
+      @shop_map_informations = data["results"]["shop"].map do |shop|
+        {
+          "name" => shop["name"],
+          "lat" => shop["lat"],
+          "lng" => shop["lng"],
+          "img" => shop["photo"]["pc"]["s"],
+          "id" => shop["id"],
+        }
+      end
+      gon.search_lat = params[:lat]
+      gon.search_lng = params[:lng]
+      gon.shop_map_informations_json = @shop_map_informations.to_json
     end
-    gon.search_lat = params[:lat]
-    gon.search_lng = params[:lng]
-    gon.shop_map_informations_json = @shop_map_informations.to_json
   end
 
   def show
